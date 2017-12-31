3ff663f1064af35fb18fe0c01c63635cfd1d0671
Henrique Melanda <hjdesigner23@gmail.com>
2017-12-31 01:35:00 -0200

    - I have refactored the JS code to be readable and removed some unnecessary things. - I changed all the  ID that was being used for DATA-JS

--- src/scripts/css-quiz.js
+++ src/scripts/css-quiz.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var myQuestions = [
+  const myQuestions = [
     {
       question: "What is 10/2?",
       answers: {
@@ -14,100 +14,74 @@
       question: "What is 30/3?",
       answers: {
         a: '3',
-        b: '5',
+        b: '50',
         c: '10'
       },
       correctAnswer: 'c'
     }
   ];
 
-  var quizContainer = document.getElementById('quiz');
-  var resultsContainer = document.getElementById('results');
-  var submitButton = document.getElementById('submit');
+  const quizContainer = document.querySelector('[data-js="quiz"]'),
+        resultsContainer = document.querySelector('[data-js="results"]'),
+        submitButton = document.querySelector('[data-js="submit"]');
 
-  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  function questions(questions){
+    const output = [];
 
-  function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+    for(let i = 0; i < questions.length; i++) {
 
-    function showQuestions(questions, quizContainer) {
-      // we'll need a place to store the output and the answer choices
-      var output = [];
-      var answers;
+      const answers = [];
 
-      // for each question...
-      for (var i = 0; i < questions.length; i++) {
+      for(let letter in questions[i].answers) {
 
-        // first reset the list of answers
-        answers = [];
-
-        // for each available answer...
-        for (var letter in questions[i].answers) {
-
-          // ...add an html radio button
-          answers.push(
-            '<label>'
-            + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-            + '<div class="checkbox"></div>'
-            + letter + ': '
-            + questions[i].answers[letter]
-            + '</label>'
-          );
-        }
-
-        // add this question and its answers to the output
-        output.push(
-          '<div class="question">' + questions[i].question + '</div>'
-          + '<div class="answers">' + answers.join('') + '</div>'
-        );
+        answers.push(`
+          <label>
+            <input type="radio" name="question${i}" value="${letter}">
+            <div class="checkbox"></div>
+            ${letter} : ${questions[i].answers[letter]}
+          </label>
+        `);
+        
       }
 
-      // finally combine our output list into one string of html and put it on the page
-      quizContainer.innerHTML = output.join('');
+      output.push(`
+        <div class="question">${questions[i].question}</div>
+        <div class="answers">${answers.join('')}</div>
+      `);
+
     }
 
-
-    function showResults(questions, quizContainer, resultsContainer) {
-
-      // gather answer containers from our quiz
-      var answerContainers = quizContainer.querySelectorAll('.answers');
-
-      // keep track of user's answers
-      var userAnswer = '';
-      var numCorrect = 0;
-
-      // for each question...
-      for (var i = 0; i < questions.length; i++) {
-
-        // find selected answer
-        userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-        // if answer is correct
-        if (userAnswer === questions[i].correctAnswer) {
-          // add to the number of correct answers
-          numCorrect++;
-
-          // color the answers green
-          answerContainers[i].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else {
-          // color the answers red
-          answerContainers[i].style.color = 'red';
-        }
-      }
-
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
-
-    // on submit, show results
-    submitButton.onclick = function () {
-      showResults(questions, quizContainer, resultsContainer);
-    }
+    quizContainer.innerHTML = output.join('');
 
   }
-})();
 
+  function result(questions) {
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    
+    let userAnswer = '';
+    let numCorrect = 0;
+
+    for(let i = 0; i < questions.length; i++) {
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+      if( userAnswer === questions[i].correctAnswer ) {
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }else{
+        answerContainers[i].style.color = 'red';
+      }
+
+    }
+
+    resultsContainer.innerHTML = `${numCorrect} de ${questions.length}`;
+
+  }
+
+  questions(myQuestions);
+
+  submitButton.addEventListener('click', function(){
+    result(myQuestions);
+  });
+
+})();
